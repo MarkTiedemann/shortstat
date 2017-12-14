90029b80a9531503f02372dc4d925675800498ea
Mark Tiedemann <www.marktiedemann@gmail.com>
2017-12-14 02:45:45 +0100

    Improve test util

--- src/test.ts
+++ src/test.ts
@@ -4,23 +4,30 @@ import chalk from 'chalk'
 const isAssertionError = (err: any): err is AssertionError =>
   err && err.code === 'ERR_ASSERTION'
 
-const logAssertionError = (message: string, err: AssertionError) => {
-  const lines = [
-    `AssertionError: ${message}`,
-    chalk.red(`- ${JSON.stringify(err.expected, null, 4)}`),
-    chalk.green(`+ ${JSON.stringify(err.actual, null, 4)}`)
-  ]
-  console.error('\n' + lines.join('\n') + '\n')
+const format = (message: string, ...stack: string[]) => {
+  const lines = [`AssertionError: ${message}`, ...stack]
+  return '\n' + lines.join('\n') + '\n'
 }
+
+const stringify = (x: any) => JSON.stringify(x, null, 4)
+
+const formatAssertionErrorStack = (err: AssertionError) => [
+  chalk.red(`- ${stringify(err.expected)}`),
+  chalk.green(`+ ${stringify(err.actual)}`)
+]
+
+const formatErrorStack = (err: Error) => [
+  err.stack ? chalk.red(err.stack) : ''
+]
 
 export default <T>(message: string, actual: T, expected: T) => {
   try {
     deepStrictEqual(actual, expected)
   } catch (err) {
-    if (isAssertionError(err)) {
-      logAssertionError(message, err)
-    } else {
-      throw err
-    }
+    process.exitCode = 1
+    const formatted = isAssertionError(err)
+      ? format(message, ...formatAssertionErrorStack(err))
+      : format(message, ...formatErrorStack(err))
+    console.error(formatted)
   }
 }
